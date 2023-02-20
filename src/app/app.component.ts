a1112850e7038f5b0179fd83e486d4edeab263b9
Bill <bill.llach1801@gmail.com>
2023-02-20 16:07:48 -0500

    make the rolling dices logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dice-roller';
+
+  dicesBaseUrl = "../assets/";
+
+  dice1: string = this.dicesBaseUrl + "dice1.png";
+  dice2: string = this.dicesBaseUrl + "dice1.png";
+
+  randomDice(): void {
+    let random1: number = Math.floor(Math.random() * 6);
+    while (random1 === 0) {
+      random1 = Math.floor(Math.random() * 6);
+    }
+
+    let random2: number = Math.floor(Math.random() * 6);
+    while (random2 === 0) {
+      random2 = Math.floor(Math.random() * 6);
+    }
+
+    this.dice1 = `${this.dicesBaseUrl}dice${random1}.png`;
+    this.dice2 = `${this.dicesBaseUrl}dice${random2}.png`;
+
+    console.log(this.dice1, this.dice2);
+  }
 }
